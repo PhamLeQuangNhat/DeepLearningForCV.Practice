092b0fc0727932a7476e69917bc7065ae2175352
quangnhat <18520120@gm.uit.edu.vn>
2020-05-20 12:41:15 +0700

    chater3

--- Chapter3/ArrayImage.py
+++ Chapter3/ArrayImage.py
@@ -1,9 +1,24 @@
 import cv2
-import argparse 
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True, help="path to input image")
-ap.add_argument("-n", "--name", type=str, required=False,default="Image")
-args= vars(ap.parse_args())
-image = cv2.imread(args["dataset"])
-cv2.imshow(args["name"],image)
-cv2.waitKey(0)
+import argparse
+
+def option():
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required=True, help="path to input image")
+    ap.add_argument("-n", "--name", type=str, required=False,default="Image")
+    args= vars(ap.parse_args())
+    return args
+
+def main():
+    args = option()
+    
+    image = cv2.imread(args["dataset"])
+    print(args["name"],image.shape)
+    """
+    cv2.imshow(image)
+    cv2.waitKey(0)
+    """
+
+if __name__ == '__main__':
+    main()
+
